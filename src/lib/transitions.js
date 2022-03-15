7f865da953549ea6e511c2b159f6ee7fd700196a
Redfluffydragon <kderu1420@gmail.com>
2022-03-15 11:45:37 -0600

    Add a safeScale transition It does a fade instead if prefers-reduced-motion is true

---
+++ src/lib/transitions.js
@@ -0,0 +1,11 @@
+import { get } from "svelte/store";
+import { fade, scale } from "svelte/transition";
+import { reducedMotion } from "./stores";
+
+/**
+ * @param {Element} node
+ * @param {import("svelte/transition").FadeParams} options
+ */
+export function safeScale(node, options) {
+  return get(reducedMotion) ? fade(node, options) : scale(node, options);
+}
