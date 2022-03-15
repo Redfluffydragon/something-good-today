a7dc9cfcbf428b4f98e1ae3d323c44293569e167
Redfluffydragon <kderu1420@gmail.com>
2022-03-15 11:45:04 -0600

    Add a top-level layout to listen for prefers-reduced-motion

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { reducedMotion } from "$lib/stores";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+    $reducedMotion = mediaQuery.matches;
+
+    mediaQuery.addEventListener('change', () => {
+      $reducedMotion = mediaQuery.matches;
+    });
+  });
+</script>
+
+<slot />
