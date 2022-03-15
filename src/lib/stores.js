1236febaa09fed0ccc5139df12b40b2ff1685fcb
Redfluffydragon <kderu1420@gmail.com>
2022-03-15 16:09:36 -0600

    Move Firebase config to a store

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,7 +1,16 @@
-import { writable } from "svelte/store";
+import { readable, writable } from "svelte/store";
 
 export const reducedMotion = writable(false);
 
-export let projects = writable([]);
+export const projects = writable([]);
 
-export let name = writable('Kai');
+export const name = writable('Kai');
+
+export const firebaseConfig = readable({
+  apiKey: "AIzaSyAHCt552UgH7VJGxjiFJlH1D0iLYxuE8iQ",
+  authDomain: "something-good-70b39.firebaseapp.com",
+  projectId: "something-good-70b39",
+  storageBucket: "something-good-70b39.appspot.com",
+  messagingSenderId: "805543978877",
+  appId: "1:805543978877:web:d425996272cc609a368e14"
+});
