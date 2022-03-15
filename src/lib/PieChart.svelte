56e9f5b93d71a07b294c3850f94072f64c2f58e3
Redfluffydragon <kderu1420@gmail.com>
2022-03-15 12:06:22 -0600

    Add pie chart component To display the completed projects

---
+++ src/lib/PieChart.svelte
@@ -0,0 +1,98 @@
+<script>
+  import { Chart, PieController, ArcElement, Tooltip, Legend } from 'chart.js';
+  import { onMount } from 'svelte';
+
+  export let projects = [];
+  export let goal = 1;
+
+  let canvas;
+
+  let pieChart;
+  let data;
+  let getLabels = [];
+  let bgColors = [];
+  let sizes = [];
+  const HOVER_OFFSET = 15;
+
+  let oldLength = projects.length;
+
+  /**
+  * @param {number} goal
+  */
+  function updateData(goal) {
+    data = {
+      labels: getLabels,
+      datasets: [{
+        label: 'Today',
+        backgroundColor: bgColors,
+        hoverOffset: HOVER_OFFSET,
+        clip: 100,
+        borderWidth: 0,
+        circumference: 360 * Math.min(sizes.length / goal, 1),
+        data: sizes,
+      }],
+    };
+
+    pieChart && (pieChart.data = data);
+
+    // Don't animate unless adding or removing a project
+    // Don't animate when changing colors, for example
+    pieChart?.update(oldLength === projects.length ? 'none' : '');
+
+    oldLength = projects.length;
+  }
+
+  /**
+   * @param {any[]} projects
+   * @param {number} goal
+  */
+  function getData(projects, goal) {
+    bgColors.length = 0;
+    getLabels.length = 0;
+    for (const project of projects) {
+      getLabels = [...getLabels, project.title];
+      bgColors = [...bgColors, project.color];
+    }
+    sizes = Array(getLabels.length).fill(1);
+    sizes = Array(getLabels.length).fill(1);
+
+    updateData(goal);
+  }
+
+  $: getData(projects, goal);
+
+  onMount(() => {
+    Chart.register(PieController, ArcElement, Tooltip);
+
+    pieChart = new Chart(canvas, {
+      type: 'pie',
+      data: data,
+      options: {
+        layout: {
+          padding: HOVER_OFFSET,
+        }
+      }
+    });
+  });
+</script>
+
+<div class="relative">
+  {#if !Object.keys(projects).length}
+    <p>Nothing yet today.</p>
+  {/if}
+
+  <canvas bind:this={canvas}></canvas>
+</div>
+
+<style>
+  div {
+    max-width: 40ch;
+    margin: 0 auto;
+  }
+
+  p {
+    text-align: center;
+    position: absolute;
+    width: 100%;
+  }
+</style>
