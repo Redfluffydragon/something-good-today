8b8a5d297f5c2686271c664253e05c3e1b9ed1c2
Redfluffydragon <kderu1420@gmail.com>
2022-03-15 12:06:53 -0600

    Add edit projects component Lets you edit your projects in a modal

---
+++ src/lib/EditProjects.svelte
@@ -0,0 +1,82 @@
+<script>
+  import Modal from "./Modal.svelte";
+  import { projects } from "./stores";
+
+  let open = false;
+
+  let selectedProject = -1;
+  let newColor;
+  let newTitle = '';
+
+  function updateProject() {
+    $projects[selectedProject].color = newColor;
+    $projects[selectedProject].title = newTitle;
+  }
+</script>
+
+<button on:click={() => { open = true; }}>Edit projects</button>
+
+<Modal bind:open maxWidth="80ch">
+  <h2>Edit projects</h2>
+  <div class="flex">
+    <ul>
+      {#each $projects as project, i}
+        <li>
+          <button on:click={() => {
+            selectedProject = i;
+            newColor = project.color;
+            newTitle = project.title;
+          }}>
+            {project.title}
+          </button>
+        </li>
+      {/each}
+    </ul>
+    <form on:input={updateProject} on:submit={e => {
+      e.preventDefault();
+      open = false;
+    }}>
+      <fieldset disabled={selectedProject === -1}>
+        <div class="flex">
+          <input type="text" name="newTitle" id="newTitle" bind:value={newTitle}>
+          <div>
+            <label for="newColor">Color:</label>
+            <input type="color" name="newColor" id="newColor" bind:value={newColor}>
+          </div>
+          <button>Mark project as done</button>
+          <button>Delete project</button>
+        </div>
+      </fieldset>
+    </form>
+  </div>
+  <button on:click={() => { open = false; }}>Done</button>
+</Modal>
+
+<style>
+  .flex {
+    display: flex;
+    gap: 2ch;
+  }
+
+  ul button {
+    background: transparent;
+  }
+
+  form {
+    border: 2px solid black;
+    padding: 2ch;
+    border-radius: 16px;
+  }
+
+  fieldset {
+    display: contents;
+  }
+
+  form .flex {
+    flex-direction: column;
+  }
+
+  input[type=text] {
+    width: 40ch;
+  }
+</style>
