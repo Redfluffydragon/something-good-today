a9f66effff0c93790bf2e929af60d21a3f7cbf57
Redfluffydragon <kderu1420@gmail.com>
2022-03-15 11:40:36 -0600

    Add Card component for consistent styles

---
+++ src/lib/Card.svelte
@@ -0,0 +1,13 @@
+<div>
+  <slot />
+</div>
+
+<style>
+  div {
+    background: var(--overlay-bg);
+    border-radius: 16px;
+    box-shadow: var(--shadow-elevation-medium);
+    padding: 2ch;
+    max-width: 100%;
+  }
+</style>
