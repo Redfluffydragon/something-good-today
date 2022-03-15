b886759aae654e82a5f8d7963c1c70fe6e853556
Redfluffydragon <kderu1420@gmail.com>
2022-03-15 12:05:14 -0600

    Add Tooltip component

---
+++ src/lib/Tooltip.svelte
@@ -0,0 +1,23 @@
+<script>
+import Card from "./Card.svelte";
+
+
+</script>
+
+<div class="tooltip">
+  <Card>
+    <slot />
+  </Card>
+</div>
+
+<style>
+  .tooltip {
+    position: absolute;
+    top: calc(100% + 1ch);
+    width: max-content;
+  }
+
+  :global(.tooltip) {
+    color: red;
+  }
+</style>
