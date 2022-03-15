a00226a1e248a64ba556ad514cc493b58e6ae0fa
Redfluffydragon <kderu1420@gmail.com>
2022-03-15 11:46:06 -0600

    Add a Popup component Uses the safeScale transition

---
+++ src/lib/Popup.svelte
@@ -0,0 +1,64 @@
+<script>
+  import { browser } from '$app/env';
+  import { onDestroy, onMount } from 'svelte';
+  import Card from './Card.svelte';
+  import { safeScale } from './transitions';
+
+  export let button;
+  export let open = false;
+
+  export let position = 'right';
+
+  function closeOnClick(e) {
+    setTimeout(() => {
+      if (!e.target.closest('.popup') && e.target !== button) {
+        open = false;
+      }
+    }, 100);
+  }
+
+  /**
+  * @param {{ key: string; }} e
+  */
+  function escape(e) {
+    if (e.key === 'Escape') {
+      open = false;
+    }
+  }
+
+  onMount(() => {
+    document.addEventListener('click', closeOnClick, false);
+    addEventListener('keydown', escape, false);
+  });
+
+  onDestroy(() => {
+    if (!browser) return;
+
+    document.removeEventListener('click', closeOnClick, false);
+    removeEventListener('keydown', escape, false);
+  });
+</script>
+
+{#if open}
+  <div
+    transition:safeScale={{ duration: 300 }}
+    class='popup {position}'
+    style='left: {button.offsetWidth + 15}px;'
+  >
+    <Card>
+      <slot />
+    </Card>
+  </div>
+{/if}
+
+<style>
+  .popup {
+    position: absolute;
+    width: max-content;
+  }
+
+  .right {
+    right: 0;
+    transform: translateY(-50%);
+  }
+</style>
