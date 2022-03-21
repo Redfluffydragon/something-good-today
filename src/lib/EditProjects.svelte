56d309b0e35613edc98b02ff1a18fa8757c111b1
Redfluffydragon <kderu1420@gmail.com>
2022-03-21 10:15:58 -0600

    Fix edit projects modal spacing

--- src/lib/EditProjects.svelte
+++ src/lib/EditProjects.svelte
@@ -41,28 +41,30 @@
 
 <Modal bind:open maxWidth="80ch">
   <h2>Edit projects</h2>
-  <div class="flex">
-    {#if $user.projects?.length}
-      <DraggableProjectsList bind:items={$user.projects} bind:selectedProject bind:newColor bind:newTitle />
-    {/if}
-    <form on:input={updateProject} on:submit={e => {
-      e.preventDefault();
-      open = false;
-    }}>
-      <fieldset disabled={selectedIndex === -1}>
-        <div class="flex">
-          <input type="text" name="newTitle" id="newTitle" bind:value={newTitle}>
-          <div>
-            <label for="newColor">Color:</label>
-            <input type="color" name="newColor" id="newColor" bind:value={newColor}>
+  <div class="flex column">
+    <div class="flex">
+      {#if $user.projects?.length}
+        <DraggableProjectsList bind:items={$user.projects} bind:selectedProject bind:newColor bind:newTitle />
+      {/if}
+      <form on:input={updateProject} on:submit={e => {
+        e.preventDefault();
+        open = false;
+      }}>
+        <fieldset disabled={selectedIndex === -1}>
+          <div class="flex column">
+            <input type="text" name="newTitle" id="newTitle" bind:value={newTitle}>
+            <div>
+              <label for="newColor">Color:</label>
+              <input type="color" name="newColor" id="newColor" bind:value={newColor}>
+            </div>
+            <button on:click={finishProject}>Mark project as finished</button>
+            <button class="danger-btn" on:click={deleteProject}>Delete project</button>
           </div>
-          <button on:click={finishProject}>Mark project as finished</button>
-          <button class="danger-btn" on:click={deleteProject}>Delete project</button>
-        </div>
-      </fieldset>
-    </form>
+        </fieldset>
+      </form>
+    </div>
+    <div class="flex"><button on:click={() => { open = false; }}>Done</button></div>
   </div>
-  <div class="flex"><button on:click={() => { open = false; }}>Done</button></div>
 </Modal>
 
 <style>
@@ -70,8 +72,11 @@
     display: flex;
     flex-wrap: wrap;
     gap: 2ch;
-    margin-bottom: 2ch;
     justify-content: center;
+  }
+
+  .column {
+    flex-direction: column;
   }
 
   form {
@@ -82,10 +87,6 @@
 
   fieldset {
     display: contents;
-  }
-
-  form .flex {
-    flex-direction: column;
   }
 
   input[type=text] {
