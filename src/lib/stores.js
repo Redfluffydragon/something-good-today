40b4ae841a723c94946e26d73d24fb7901a44bb5
Redfluffydragon <kderu1420@gmail.com>
2022-03-23 21:00:59 -0600

    Update demo user data for new structure

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -23,55 +23,41 @@ export const demoUserData = readable({
   goal: "3",
   name: "Demo user",
   nextId: 6,
-  projects: [
-    {
-      color: "#6119ac",
-      id: 4,
-      title: "Sample project",
-    },
-    {
+  projects: {
+    0: {
       color: "#ff2962",
       id: 0,
       title: "Write",
     },
-    {
+    1: {
       color: "#9573c7",
       id: 1,
       title: "Practice instrument",
     },
-    {
+    2: {
       color: "#f5d51e",
       id: 2,
       title: "Yes",
     },
-    {
-      color: "#4aa260",
-      id: 5,
-      title: "Stuff",
-    }
-  ],
-  finishedProjects: [
-    {
+    3: {
       color: "#e41b57",
       id: 3,
       title: "Things",
     },
-  ],
-  today: [
-    {
+    4: {
       color: "#6119ac",
       id: 4,
       title: "Sample project",
     },
-    {
-      color: "#9573c7",
-      id: 1,
-      title: "Practice instrument",
-    },
-    {
-      color: "#f5d51e",
-      id: 2,
-      title: "Yes",
-    },
-  ]
-})
+    5: {
+      color: "#4aa260",
+      id: 5,
+      title: "Stuff",
+    }
+  },
+  activeProjects: [0, 1, 2, 4, 5],
+  finishedProjects: [3],
+  deletedProjects: [],
+  today: [4, 1, 2],
+  history: [],
+});
