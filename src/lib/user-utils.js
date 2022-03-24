baa44ebf28bd3f9e628be52b0a1404758844c052
Redfluffydragon <kderu1420@gmail.com>
2022-03-23 20:06:47 -0600

    Add an addToHistory function Should add yesterday's completed projects to the history when it's called

---
+++ src/lib/user-utils.js
@@ -0,0 +1,21 @@
+import dayjs from "dayjs";
+import { get } from "svelte/store";
+import { user } from "./stores";
+
+export function addToHistory() {
+  if (dayjs().isAfter(get(user).lastUpdated, 'day')) {
+    console.log('NEW DAY!');
+    user.update(user => {
+      user.history.push({
+        date: user.lastUpdated,
+        projects: user.today,
+        goal: user.goal,
+      });
+
+      user.today = [];
+      user.lastUpdated = Date.now();
+
+      return user;
+    });
+  }
+}
