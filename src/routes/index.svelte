33c57641c148a21e61ff48840a624727157dead3
Redfluffydragon <kderu1420@gmail.com>
2022-04-01 10:29:15 -0600

    Use $profile.displayName  instead of $user.name

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
   import EditProjects from '$lib/EditProjects.svelte';
   import PieChart from '$lib/PieChart.svelte';
   import FinishedProjects from '$lib/FinishedProjects.svelte';
-  import { loggedIn, shouldUpdate, user } from '$lib/stores';
+  import { loggedIn, profile, shouldUpdate, user } from '$lib/stores';
   import { beforeNavigate } from '$app/navigation';
   import { browser } from '$app/env';
   import { doc, setDoc } from 'firebase/firestore';
@@ -41,7 +41,7 @@
         { merge: true }
       );
 
-      console.log('Updated', user.name);
+      console.log('Updated', $profile.displayName);
     } catch (err) {
       console.log('Failed to update:', err);
     }
