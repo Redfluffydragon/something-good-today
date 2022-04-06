74a548af1ab2b81e16c7a8b79a9c4c306e5f211e
Redfluffydragon <kderu1420@gmail.com>
2022-04-06 10:46:46 -0600

    Try to make service worker actually respond with cached files (again)

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,7 +4,6 @@ const worker = self;
 const CACHE_NAME = `static-cache-${version}`;
 
 const to_cache = build.concat(files);
-const staticAssets = new Set(to_cache);
 
 worker.addEventListener('install', event => {
   event.waitUntil(
@@ -25,39 +24,19 @@ worker.addEventListener('activate', event => {
   );
 });
 
-/**
- * Fetch the asset from the network and store it in the cache.
- * Fall back to the cache if the user is offline.
- */
-async function fetchAndCache(request) {
-  const cache = await caches.open(CACHE_NAME);
-
-  try {
-    const response = await fetch(request);
-    cache.put(request, response.clone());
-    return response;
-  } catch (err) {
-    const response = await cache.match(request);
-    if (response) return response;
-
-    throw err;
-  }
-}
-
 worker.addEventListener('fetch', event => {
   if (event.request.mode !== 'navigate') {
     // Not a page navigation, bail.
     return;
   }
 
-  const url = new URL(event.request.url);
-  const isStaticAsset = staticAssets.has(url.pathname);
-
   event.respondWith(
-    (async () => {
-      const cachedAsset = isStaticAsset && (await caches.match(event.request));
-
-      return cachedAsset || fetchAndCache(event.request);
-    })()
+    fetch(event.request)
+      .catch(async () => {
+        return caches.open(CACHE_NAME)
+          .then(cache => {
+            return cache.match(event.request);
+          });
+      })
   );
 });
