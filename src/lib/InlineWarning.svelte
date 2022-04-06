e42d7af7c35d24cf1f440adea6f218a3764f03cb
Redfluffydragon <kderu1420@gmail.com>
2022-04-05 19:09:15 -0600

    Add inline warning component for form validation

---
+++ src/lib/InlineWarning.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let show;
+</script>
+
+{#if show}
+  <button
+    class="clear-btn"
+    title="Click to dismiss"
+    on:click={() => {
+      show = false;
+    }}
+  >
+    <p><slot /></p>
+  </button>
+{/if}
+
+<style>
+  button {
+    padding: 0;
+    width: 100%;
+  }
+
+  p {
+    text-align: center;
+    border-radius: 1ch;
+    background: #a81b1b;
+    color: white;
+  }
+</style>
