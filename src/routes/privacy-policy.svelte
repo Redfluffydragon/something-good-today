b4cd38486de6423bde65a615a4685eed4289b805
Redfluffydragon <kderu1420@gmail.com>
2022-04-07 12:06:00 -0600

    Add a title to the privacy policy page

--- src/routes/privacy-policy.svelte
+++ src/routes/privacy-policy.svelte
@@ -1,3 +1,11 @@
+<script>
+  import { titleSuffix } from '$lib/stores';
+</script>
+
+<svelte:head>
+  <title>Privacy Policy {titleSuffix}</title>
+</svelte:head>
+
 <main>
   <h1>Privacy Policy</h1>
   <p>All this site collects is whatever data you give it when you sign up. I'll never email you, because I'm too lazy to set that up.</p>
