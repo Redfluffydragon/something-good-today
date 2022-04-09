f51829c2d6882dd53109e0445be1f00697986964
Redfluffydragon <kderu1420@gmail.com>
2022-04-09 13:41:13 -0600

    Add a CSP

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,14 @@ const config = {
   kit: {
     adapter: adapter(),
     inlineStyleThreshold: 1024,
+    csp: {
+      mode: 'auto',
+      directives: {
+        'script-src': ['self', 'sha256-o92fbhqsc3TjFajrat1E9BW6KGyt6RdOl/IFHamzrxY='],
+        'object-src': ['none'],
+        'base-uri': ['none'],
+      },
+    },
   },
 };
 
