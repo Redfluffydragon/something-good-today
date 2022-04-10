70907f482412735deb3b71e9b4b8dee5d8abfe50
Redfluffydragon <kderu1420@gmail.com>
2022-04-10 16:34:54 -0600

    Put 'self' back in CSP script-src

--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,7 @@ const config = {
       mode: 'auto',
       directives: {
         'script-src': [
+          'self',
           'sha256-o92fbhqsc3TjFajrat1E9BW6KGyt6RdOl/IFHamzrxY=',
           'https://apis.google.com/js/api.js',
           'https://apis.google.com/_/scs/abc-static/_/js/k=gapi.lb.en.NnK9YPjtg-w.O/m=gapi_iframes/rt=j/sv=1/d=1/ed=1/rs=AHpOoo9KePDGVlGjp-rlXwDM1kUO2Eh4gg/cb=gapi.loaded_0',
