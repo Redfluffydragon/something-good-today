320e63d1d499aa3db92df1d61c15dbecdf9f5ab0
Redfluffydragon <kderu1420@gmail.com>
2022-04-10 15:36:42 -0600

    Fix CSP to allow logging in with Google

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,12 @@ const config = {
     csp: {
       mode: 'auto',
       directives: {
-        'script-src': ['self', 'sha256-o92fbhqsc3TjFajrat1E9BW6KGyt6RdOl/IFHamzrxY=', 'unsafe-inline'],
+        'script-src': [
+          'sha256-o92fbhqsc3TjFajrat1E9BW6KGyt6RdOl/IFHamzrxY=',
+          'https://apis.google.com/js/api.js',
+          'https://apis.google.com/_/scs/abc-static/_/js/k=gapi.lb.en.NnK9YPjtg-w.O/m=gapi_iframes/rt=j/sv=1/d=1/ed=1/rs=AHpOoo9KePDGVlGjp-rlXwDM1kUO2Eh4gg/cb=gapi.loaded_0',
+          'unsafe-inline',
+        ],
         'object-src': ['none'],
         'base-uri': ['none'],
       },
