2331506e74abf750cc3a2fd8acc73afaa89aa6ec
Kai-Shen Deru <kderu1420@gmail.com>
2022-12-14 17:33:19 -0700

    svelte-migrate: change $app/env to $app/environment

--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { browser } from '$app/env';
+  import { browser } from '$app/environment';
   import { onDestroy, onMount } from 'svelte';
   import Card from './Card.svelte';
   import { safeScale } from './transitions';
